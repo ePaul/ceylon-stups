425a0ffa2d8274fc9fa1a132ca177ef68d605db0
Paul Ebermann <paul.ebermann@zalando.de>
2015-12-15 11:09:47 +0100

    make cache readable for unprivileged user (and correct typo).
    This makes it possible to run this container as nobody.
    Also, some reworkings of the build script.

--- helloworld/build.sh
+++ helloworld/build.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
 
-MODULE_VERSION=$( egrep 'module .* {' source/org/zalando/ceylon_stups/helloWorld/module.ceylon | sed -r 's/.*"([^"]+)" \{/\1/' )
+set -o errexit
 
-ceylon compile --cacherep=./cache org.zalando.ceylon_stups.helloWorld
+CEYLON_MODULE_NAME=org.zalando.ceylon_stups.helloWorld
+MODULE_VERSION=$( egrep 'module .* {' source/org/zalando/ceylon_stups/helloWorld/module.ceylon | sed -r 's/.*"([^"]+)" \{/\1/' )
+DOCKER_ARTIFACT_NAME=pierone.stups.zalan.do/hackweek/ceylon-stups-helloworld
+
+rm -r ./cache
+rm -r ./modules
+echo "Ceylon building ..."
+ceylon compile --cacherep=./cache $CEYLON_MODULE_NAME
+echo "... Ceylon build done."
 scm-source
 
-sudo docker build -t pierone.stups.zalan.do/hackweek/ceylon-stups-helloworld:$MODULE_VERSION .
-sudo docker push pierone.stups.zalan.do/hackweek/ceylon-stups-helloworld:$MODULE_VERSION
+sudo docker build -t $DOCKER_ARTIFACT_NAME:$MODULE_VERSION .
+echo ""
+echo "built $DOCKER_ARTIFACT_NAME:$MODULE_VERSION"
+
+if [[ "$1" = "-push" ]]
+then
+  echo "pushing to registry ..." 
+  sudo docker push $DOCKER_ARTIFACT_NAME:$MODULE_VERSION
+else
+  echo "omitted pushing."
+fi 
